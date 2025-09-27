bcfdc3f9021f699c976322434781014e0a5d4207
ZenAyush <61388231+ZenAyush@users.noreply.github.com>
2025-09-27 17:46:35 +0530

    Update ig downloader.py

--- ig downloader.py
+++ ig downloader.py
@@ -5,11 +5,11 @@ def download_instagram_video(video_url, output_path="PASTE THE FOLDER LOCATION")
     """
     Downloads an Instagram video, reel, or post using yt-dlp.
     """
-    command = f'yt-dlp -o "{output_path}/%(title)s.%(ext)s" {video_url}'
+    # Add unique ID in filename to avoid overwrite
+    command = f'yt-dlp -o "{output_path}/%(uploader)s - %(id)s.%(ext)s" {video_url}'
     subprocess.run(command, shell=True)
     print("✅ Download Completed!")
 
 if __name__ == "__main__":
     video_url = input("Enter the Instagram video URL: ")
     download_instagram_video(video_url)
-
